d55ce8ecc32376965fa46c59952c5b1d6e19dbc7
Matthias Hölzl <tc@xantira.com>
2026-03-09 11:40:57 +0100

    Fix quotation style

--- main/utils.py
+++ main/utils.py
@@ -1,8 +1,8 @@
 # utils.py
 
 def say_hello(name="app.py"):
-    print(f'Hello from {name}!')
+    print(f"Hello from {name}!")
 
 
 def helper():
-    print('Hello from utils.py')
+    print("Hello from utils.py")
